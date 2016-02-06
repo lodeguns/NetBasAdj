15513d4fa68314c56b0b07a34ef59731dde5a6c9
Francesco Bardozzo <lodeguns@dr.com>
2016-02-06 12:17:23 +0100

    Create NetBasAdj-Example.R

---
+++ NetBasAdj-Example.R
@@ -0,0 +1,46 @@
+########################################################################
+#  Multi Omic Oscillations Networks
+#
+#  Network Based Adjacency (NBA)  - Example of Use
+#
+#  Francesco Bardozzo, Pietro Liò, Roberto Tagliaferri
+#
+########################################################################
+#Load DataSet
+#https://github.com/lodeguns/NetBasAdj/blob/master/osc.paths.list.RData
+
+
+load('./osc.paths.list.Rdata')
+
+#Load Functions
+#https://tonybreyal.wordpress.com/2011/11/24/source_https-sourcing-an-r-script-from-github/
+source_https <- function(url, ...) {
+  require(RCurl)
+  sapply(c(url, ...), function(u) {
+    eval(parse(text = getURL(u, followlocation = TRUE, 
+    cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
+  })
+}
+
+source_https("https://github.com/lodeguns/NetBasAdj/raw/master/NetBasAdj.R")
+
+
+########################################################################
+#Run the example for ingoing and outgoing degree. At the index 1 of the
+#dataset "osc.paths.list" are tabulated the omics values for the metabolic
+#network Glycolysis of Escherichia coli K-12 MG1655.
+
+oscill.org = "eco"
+
+glyco.w.in  <- osc.get.list.w(osc.paths.list[1], oscill.org, "in")
+osc.select.w.all(glyco.w.in)
+
+glyco.w.out <- osc.get.list.w(osc.paths.list[1], oscill.org, "out")
+osc.select.w.all(glyco.w.out)
+
+
+All the datas are publicly available and comes from KEGG, NCBI, PaXDB.
+########################################################################
+
+
+
